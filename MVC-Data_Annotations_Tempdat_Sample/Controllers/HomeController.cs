fba683a003bd308ad00a42df7dffe02e6cb34974
vivek k <99602815+NETTraining122@users.noreply.github.com>
2022-02-28 23:27:08 +0530

    Add files via upload

---
+++ MVC-Data_Annotations_Tempdat_Sample/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Employee EM)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["mydata"] = EM; 
+                return RedirectToAction("Index2","Home");
+            
+            }
+            return View("Index");
+        }
+        public ActionResult Index2()
+        {
+            Employee data = TempData["mydata"] as Employee;
+           // TempData.Keep();
+           return View(data);
+
+            }
+            
+        }
+
+    }
+
