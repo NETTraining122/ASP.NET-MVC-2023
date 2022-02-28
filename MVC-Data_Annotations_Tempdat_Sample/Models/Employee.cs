d3c0d4390b4bfdf85024f1992bded3b778a09f49
vivek k <99602815+NETTraining122@users.noreply.github.com>
2022-02-28 23:36:46 +0530

    Add files via upload

---
+++ MVC-Data_Annotations_Tempdat_Sample/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcApplication1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        //add all other validation
+        [Required(ErrorMessage="Plz enter the name")]
+        public string Name {get; set; }
+        [Required(ErrorMessage = "Plz enter the city")]
+        public string City { get; set; }
+       
+    }
+}
