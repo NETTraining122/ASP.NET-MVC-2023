b244d26d04c1218c9241daa78f51343eb755d642
vivek k <99602815+NETTraining122@users.noreply.github.com>
2022-03-01 13:17:08 +0530

    Add files via upload

---
+++ EF-DbFirstApproach/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication2.Models;
+
+namespace MvcApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            using (var myentity = new Database1Entities1())
+            {
+
+                List<Table> um = myentity.Tables.ToList();
+
+                return View(um);
+            }
+
+        }
+
+
+    }
+}
