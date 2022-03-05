6fb44e973b8961500d054a64c40a2b745657b5ce
vivek k <99602815+NETTraining122@users.noreply.github.com>
2022-03-05 09:09:04 +0530

    Add files via upload

---
+++ CustomHelper/my11/helper11.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CustomHelpers.my11
+{
+    public static class helper11
+    {
+        public static MvcHtmlString MyList(this HtmlHelper html, string list)
+        {
+
+        return MvcHtmlString.Create( @"<li>" + list + "</li>" );
+        }
+        public static MvcHtmlString MyImage(this HtmlHelper helper, string src, string altText, string height,string width)
+        {
+
+            var builder = new TagBuilder("img");
+
+            builder.MergeAttribute("src", src);
+
+            builder.MergeAttribute("alt", altText);
+
+            builder.MergeAttribute("height", height);
+
+            builder.MergeAttribute("height", width);
+
+            return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
+
+        }
+    }
+}
