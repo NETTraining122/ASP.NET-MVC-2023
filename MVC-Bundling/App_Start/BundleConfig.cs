328853de3ab8c5d6080c43bc6bdc42cd797efa7d
vivek k <99602815+NETTraining122@users.noreply.github.com>
2022-03-05 08:32:55 +0530

    Add files via upload

---
+++ MVC-Bundling/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Bundle
+{
+   public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+                
+    }
+}
+}
